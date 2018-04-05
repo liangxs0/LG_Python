9ac3d9ae4dec7a9b7dc353c4710faa9675a29e63
liangxs0 <1033808656@qq.com>
2018-04-05 12:58:32 +0800

    success

--- LG_code.py
+++ LG_code.py
@@ -1,6 +1,53 @@
 #python3.6
 import requests
 import time
+import xlwt
+from xlrd import open_workbook
+import os
+from os.path import join
+from xlutils.copy import copy
+from datetime import datetime
+
+
+
+
+def set_style(font_name, font_height, bold=False):
+    style = xlwt.XFStyle()
+
+    font = xlwt.Font()
+    font.name = font_name  # 'Times New Roman'
+    font.height = font_height
+    font.bold = bold
+    font.colour_index = 4
+
+    borders = xlwt.Borders()
+    borders.left = 6
+    borders.right = 6
+    borders.top = 6
+    borders.bottom = 6
+
+    style.font = font
+    style.borders = borders
+    return style
+
+#建表函数
+def write_to_excel_xlwt():
+    '''Write content to a new excel'''
+    row = 0
+    lin = 0
+    new_workbook = xlwt.Workbook()
+    new_sheet = new_workbook.add_sheet("LG")
+    # write cell with style
+    style0 = set_style('Times New Roman',200,200)
+    new_sheet.write(0, 0, 'companyFullName',style0)
+    new_sheet.write(0, 1, 'jobNature',style0)
+    new_sheet.write(0, 2, 'firstType',style0)
+    new_sheet.write(0, 3, 'education',style0)
+    new_sheet.write(0, 4, 'workYear',style0)
+    new_sheet.write(0, 5, 'salary', style0)
+    new_sheet.write(0, 6, 'positionName',style0)
+    new_sheet.write(0, 7, 'companySize',style0)
+    new_workbook.save(r"LG数据信息.xls")  # if change to xlsx,then open failed
 def get_info(type):
     #获取网址信息
     url = "https://www.lagou.com/jobs/positionAjax.json?city=%E5%8C%97%E4%BA%AC&needAddtionalResult=false&isSchoolJob=0"
@@ -27,29 +74,72 @@ def get_info(type):
         #提交数据
         postData = {'first':'false',
                    'pn': str(n),
-                   'kd': type}
+                   'kd': type
+                    }
         # 提交参数到服务器，返回数据信息---》招聘信息
         html = requests.post(url, data = postData, headers = header)
         print(html)
         for m in range(len(html.json()['content']['positionResult']['result'])):
-           companyFullName =  html.json()['content']['positionResult']['result'][m]['companyFullName']
-            #信息写入文件
-           ff = open('ff.txt','a+')
-           ff.write(companyFullName+'\n')
-
+            companyFullName =  html.json()['content']['positionResult']['result'][m]['companyFullName']
+            jobNature = html.json()['content']['positionResult']['result'][m]['jobNature']
+            firstType = html.json()['content']['positionResult']['result'][m]['firstType']
+            education = html.json()['content']['positionResult']['result'][m]['education']
+            workYear = html.json()['content']['positionResult']['result'][m]['workYear']
+            salary = html.json()['content']['positionResult']['result'][m]['salary']
+            positionName = html.json()['content']['positionResult']['result'][m]['positionName']
+            companySize = html.json()['content']['positionResult']['result'][m]['companySize']
+            ff=open('ff.txt','a+')
+            ff.write(companyFullName+'\n')
+            ff.write(jobNature+'\n')
+            ff.write(firstType + '\n')
+            ff.write(education+'\n')
+            ff.write(workYear+'\n')
+            ff.write(salary+'\n')
+            ff.write(positionName+'\n')
+            ff.write(companySize+'\n')
+            ff.close()
+def index_data(row,line,con):
+    source = r"‪G:\python_work\LG_data\LG数据信息.xls"
+    rb = open_workbook("LG数据信息.xls",formatting_info=True)
+    w = copy(rb)
+    w.get_sheet(0).write(row,line,con)
+    time.sleep(0.5)
+    w.save("LG数据信息.xls")
 if __name__== '__main__':
-    ff = open('ff.txt', 'a+')
-    ff.write('\n'+"-----------------python----------------" + '\n')
-    ff.close()
-    type = 'python'
-    get_info(type)
-    ff = open('ff.txt', 'a+')
-    ff.write('\n'+"-----------------java----------------" + '\n')
-    ff.close()
-    type = 'java'
-    get_info(type)
-    ff = open('ff.txt', 'a+')
-    ff.write('\n'+"-----------------c----------------" + '\n')
-    ff.close()
-    type = 'c'
-    get_info(type)
+
+    i = 1
+    j = 1
+    b = 1
+    a = 0
+    c = 0
+    if(j == 0):
+        write_to_excel_xlwt()
+
+    if (i==0):
+        type = 'python'
+        get_info(type)
+
+        type = 'java'
+        get_info(type)
+
+        type = 'c'
+        get_info(type)
+    file = open("ff.txt","rU")
+    count = len(file.readlines())
+    file.close()
+    print(count)
+
+    file = open("ff.txt")
+    for a in range(count+1):
+        if (b > count//8):
+            break
+        line = file.readline()
+        print ( b, c)
+        index_data(b,c,line)
+        print(line)
+        c = c + 1
+        if(c == 8):
+            c = 0
+            b = b+1
+
+    file.close()
