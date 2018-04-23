88747674fda1b24a6d5e3cbf16692a33fc69ba3f
liangxs0 <1033808656@qq.com>
2018-04-23 15:54:49 +0800

    修复file.write()的中断的问题

--- LG_py_new.py
+++ LG_py_new.py
@@ -4,8 +4,9 @@ import requests
 import re
 import os
 import time
-
+import sys
 import datetime
+import io
 
 def get_info(type,w_type,city):
 
@@ -22,7 +23,7 @@ def get_info(type,w_type,city):
         'Cookie': '_ga=GA1.2.1527602413.1523154566; user_trace_token=20180408102928-a9b14cdc-3ad4-11e8-b665-525400f775ce; LGUID=20180408102928-a9b150b4-3ad4-11e8-b665-525400f775ce; _gid=GA1.2.549101232.1523154566; LG_LOGIN_USER_ID=4252bc12ed7ecaa633a1c6e4e82bfe3a82fb7917c41f40b3e460b222b20daf23; showExpriedIndex=1; showExpriedCompanyHome=1; showExpriedMyPublish=1; hasDeliver=0; index_location_city=%E5%85%A8%E5%9B%BD; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22162a333752849e-0ec92ae2c10d8a-b34356b-1327104-162a3337529348%22%2C%22%24device_id%22%3A%22162a333752849e-0ec92ae2c10d8a-b34356b-1327104-162a3337529348%22%7D; sajssdk_2015_cross_new_user=1; JSESSIONID=ABAAABAABEEAAJACEB0215A69270A9E4C8E22A2F0349CCF; _gat=1; LGSID=20180408113541-e996bcad-3add-11e8-b674-525400f775ce; PRE_UTM=; PRE_HOST=; PRE_SITE=; PRE_LAND=https%3A%2F%2Fwww.lagou.com%2Fjobs%2Flist_python%3Fpx%3Ddefault%26city%3D%25E5%258C%2597%25E4%25BA%25AC; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1523154566,1523154932,1523158539; _putrc=B02BCA36DD54FD83123F89F2B170EADC; login=true; unick=%E6%A2%81%E6%99%93%E5%A3%B0; gate_login_token=999aba3baef6137fab5e4c8161a0f357a93ae10da427829554e092278e20cb2f; LGRID=20180408114330-01384ae0-3adf-11e8-b677-525400f775ce; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1523159008; SEARCH_ID=f2ed24e259bd4738bb6ff0a090197cda',
         'Host': 'www.lagou.com',
         'Origin': 'https://www.lagou.com',
-        'Referer': 'https://www.lagou.com/jobs/list_python%E7%88%AC%E8%99%AB?px=default&city=%E5%8C%97%E4%BA%AC',
+        #'Referer': 'https://www.lagou.com/jobs/list_python%E7%88%AC%E8%99%AB?px=default&city=%E5%8C%97%E4%BA%AC',
         'Referer': 'https://www.lagou.com/jobs/list_'+w_type+'+?px=default&city='+city,
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
         'X-Anit-Forge-Code': '0',
@@ -62,7 +63,8 @@ def get_info(type,w_type,city):
             #district = html.json()['content']['positionResult']['result'][m]['district']
 
         file = open('ff.txt', 'a+')
-        file.write(companyFullName+'\t'+jobNature+'\t'+education+'\t'+workYear+'\t'+salary+'\t'+positionName+'\t'+companySize+'\t')
+        file.write(companyFullName+'\t'+jobNature+'\t'+education+'\t')
+        file.write(workYear+'\t'+salary+'\t'+positionName+'\t'+companySize+'\t')
         file.write(city+'\t'+financeStage+'\t'+firstType)
         file.write('\t'+str(formatCreateTime)+'\n')
         file.close()
@@ -100,7 +102,7 @@ def data_process(formatCreateTime):
 
 if __name__ == '__main__':
     #Type_l = ['java', 'python', 'C++', 'C', '物联网', '大数据', '机器学习', 'JS', 'PHP', 'HTML']
-
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf8')
     Type_l = ['java', 'python', 'C++', 'C', '嵌入式','物联网','大数据']
     w_type = {'java':'java',
               'python':'python',
@@ -110,6 +112,7 @@ if __name__ == '__main__':
               '物联网':'%E7%89%A9%E8%81%94%E7%BD%91',
               '大数据':'%E5%A4%A7%E6%95%B0%E6%8D%AE%E3%80%81'
               }
+    city = ['北京','上海','广州','深圳','杭州','成都','南京','武汉','西安','厦门','太原']
     City = {'北京':'%E5%8C%97%E4%BA%AC',
             '上海':'%E4%B8%8A%E6%B5%B7',
             '广州':'%E5%B9%BF%E5%B7%9E',
@@ -119,7 +122,8 @@ if __name__ == '__main__':
             '南京':'%E5%8D%97%E4%BA%AC',
             '武汉':'%E6%AD%A6%E6%B1%89',
             '西安':'%E8%A5%BF%E5%AE%89',
-            '厦门':'%E5%8E%A6%E9%97%A8'
+            '厦门':'%E5%8E%A6%E9%97%A8',
+            '太原':'%E5%A4%AA%E5%8E%9F'
             }
     date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     print(date)
